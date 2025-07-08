5ceddf066f4b15190e44ceb26ec021bdba174ae5
Brendan Linn <brendan.linn@gmail.com>
2025-07-08 07:14:19 -0700

    initial commit: minimal golang server.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ubehebe/r2a
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	slog.Info("hello",
+		slog.String("method", r.Method),
+		slog.String("path", r.URL.Path))
+	fmt.Fprintf(w, "hello %s", r.URL.Path)
+}
+
+func main() {
+	http.Handle("/", http.HandlerFunc(hello))
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
